a596feddcc29878f4bffa8144319c95648956c33
eduardoneira <edun327@gmail.com>
2017-10-02 21:39:30 -0300

    Fix to camara when switching

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,13 +1,15 @@
-function Camera(radius = 15, speed = 0.01) {
+function Camera(radius = 15, speed = 0.005) {
   this.prev_x = 0;
   this.prev_y = 0;
-  this.alfa = 0;
-  this.beta = 0;
+  this.alfa = Math.PI/2;
+  this.beta = Math.PI/2;
   this.radius = radius;
   this.speed = speed;
   this.prev_look_at = mat4.create();
 
   this.is_mouse_down = false;
+  this.reset_position = true;
+  this.switch_mode = true;
 
   this.mouse = {
     x: 0,
@@ -47,15 +49,17 @@ function Camera(radius = 15, speed = 0.01) {
     that.mouse.y = e.clientY || e.pageY; 
   });
   
-  $('#contenedor3d').mousedown(function(event){   
+  $('#contenedor3d').mousedown(function(event) {   
     that.is_mouse_down = true;        
   });
 
-  $('body').mouseup(function(event){
+  $('body').mouseup(function(event) {
+    that.reset_position = true;
     that.is_mouse_down = false;    
   });
 
   $('#contenedor3d').bind('mousewheel', function(e){
+    debugger;
     if (e.originalEvent.wheelDelta / 120 > 0) {
       if (that.radius > 1){
         that.radius -= 0.2;   
@@ -75,24 +79,20 @@ function Camera(radius = 15, speed = 0.01) {
   });
 
   this.getViewMatrix = function() {
-    if (this.switch_mode){
-      this.switch_mode = false;
-      var center = this.modes[this.current_mode]();
-
-      var eye = [
-        center[0] + this.radius * Math.sin(this.alfa) * Math.sin(this.beta),
-        center[1] + this.radius * Math.cos(this.beta),
-        center[2] + this.radius * Math.cos(this.alfa) * Math.sin(this.beta)
-      ];
-
-      this.prev_look_at = mat4.lookAt(this.prev_look_at,
-                                      eye,
-                                      center,
-                                      [0.0,1.0,0.0]); 
+    if (this.is_mouse_down || this.switch_mode) {
+      var deltaX = 0;
+      var deltaY = 0;
       
-    } else if (this.is_mouse_down ) {
-      var deltaX = this.mouse.x - this.prev_x;
-      var deltaY = this.mouse.y - this.prev_y;
+      if (!this.reset_position) {
+        deltaX = this.mouse.x - this.prev_x;
+        deltaY = this.mouse.y - this.prev_y;
+      }
+
+      if (!this.switch_mode){
+        this.reset_position =  false;
+      }
+
+      this.switch_mode = false;
 
       this.prev_x = this.mouse.x;
       this.prev_y = this.mouse.y;
