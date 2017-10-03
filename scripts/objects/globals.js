6bc9e6c34afcc66eb0a555e0469929d302d63a41
edu <edun327@gmail.com>
2017-10-02 22:00:44 -0300

    refactor

---
+++ scripts/objects/globals.js
@@ -0,0 +1,12 @@
+var gl = null;
+var glProgram = null;
+var fragmentShader = null;
+var vertexShader = null;
+
+// Time of frames
+var time = 0.0;
+var deltaTime = 10;
+
+// Objects
+var scene = null;
+var camera = null;
